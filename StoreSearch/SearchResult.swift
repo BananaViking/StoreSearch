f5ae0184dc955e54635acdf38d9e35e3e96bdcf9
Banana Viking <bananavikingcoder@gmail.com>
2018-03-26 15:41:33 -0500

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Banana Viking on 3/26/18.
+//  Copyright © 2018 Banana Viking. All rights reserved.
+//
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
