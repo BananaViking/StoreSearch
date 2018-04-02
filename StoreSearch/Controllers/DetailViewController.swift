2b0d81203c10aa11a3f181997cd393a14d6a7cfc
Banana Viking <bananavikingcoder@gmail.com>
2018-04-02 15:21:07 -0500

    Add animations to pop-up.

--- StoreSearch/Controllers/DetailViewController.swift
+++ StoreSearch/Controllers/DetailViewController.swift
@@ -29,6 +29,7 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.clear
         view.tintColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 1)
         popupView.layer.cornerRadius = 10    //rounds the corners of the detail pop-up
         
@@ -101,6 +102,11 @@ class DetailViewController: UIViewController {
 extension DetailViewController: UIViewControllerTransitioningDelegate {
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
         return DimmingPresentationController(presentedViewController: presented, presenting: presenting)
+    }
+    
+    //employ the bounce animation for detail pop-up
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return BounceAnimationController()
     }
 }
 
